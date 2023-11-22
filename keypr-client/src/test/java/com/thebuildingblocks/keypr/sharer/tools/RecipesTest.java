1a614cc803e4ea9679b66e7b938841c4f64594c6
Jo <jo@linguafranca.org>
2023-11-22 19:57:33 +0000

    complete migration to modular structure

---
+++ keypr-client/src/test/java/com/thebuildingblocks/keypr/sharer/tools/RecipesTest.java
@@ -0,0 +1,72 @@
+package com.thebuildingblocks.keypr.sharer.tools;
+
+import com.thebuildingblocks.keypr.common.TestIds;
+import com.thebuildingblocks.keypr.sharer.Secret;
+import com.thebuildingblocks.keypr.sharer.Sharer;
+import org.derecalliance.derec.api.DeRecIdentity;
+import com.thebuildingblocks.keypr.helper.tools.TestHelperServer;
+import org.junit.*;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.KeyPair;
+import java.util.Arrays;
+
+import static com.thebuildingblocks.keypr.common.Cryptography.keyPairGenerator;
+import static com.thebuildingblocks.keypr.common.TestIds.DEFAULT_IDS;
+import static com.thebuildingblocks.keypr.common.TestIds.pemFrom;
+
+public class RecipesTest {
+    static TestHelperServer server;
+    Sharer sharer;
+    Secret secret1, secret2;
+
+    @BeforeClass
+    public static void setUpServer() throws IOException {
+        server = new TestHelperServer();
+        server.startServer(8080, TestIds.DEFAULT_IDS);
+    }
+
+    @AfterClass
+    public static void tearDownServer() {
+        server.stopServer();
+    }
+
+    @Before
+    public void setUp() {
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+        String pem = pemFrom(keyPair.getPublic());
+        // build a sharer
+        sharer = Sharer.newBuilder()
+                .id(new DeRecIdentity("Incremental Inge", "mailto:test@example.org", null, pem))
+                .keyPair(keyPair)
+                .notificationListener(Notifier::logNotification)
+                .build();
+        secret1 = sharer.newSecret("my first secret",
+                "quite hush hush".getBytes(StandardCharsets.UTF_8),
+                Arrays.asList(DEFAULT_IDS));
+        secret1.update("an updated version".getBytes(StandardCharsets.UTF_8));
+        secret2 = sharer.newSecret("my second secret",
+                "very hush hush".getBytes(StandardCharsets.UTF_8),
+                Arrays.asList(DEFAULT_IDS));
+    }
+
+    @After
+    public void tearDown() {
+        secret1.close();
+        secret2.close();
+    }
+
+
+    @Test
+    public void listVersionNumbersForHelper() {
+    }
+
+    @Test
+    public void listHelpersAndSecretsForSharer() {
+    }
+
+    @Test
+    public void listHelpersAndSecretsForSharer2() {
+    }
+}
---
+++ keypr-common/src/test/java/com/thebuildingblocks/keypr/common/UtilTest.java
@@ -0,0 +1,17 @@
+package com.thebuildingblocks.keypr.common;
+
+import org.junit.Test;
+
+import java.util.UUID;
+
+import static org.junit.Assert.*;
+
+public class UtilTest {
+    @Test
+    public void verifyUuidToFromByte() {
+        UUID u = UUID.randomUUID();
+        byte[] uBytes = Util.asBytes(u);
+        UUID u2 = Util.asUuid(uBytes);
+        assertEquals(u, u2);
+    }
+}
